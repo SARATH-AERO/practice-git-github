e15890506fa2116c8888f8c60a25d9eb856be525
Sarath Sekar <119506630+SARATH-AERO@users.noreply.github.com>
2023-03-21 13:54:12 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("2nd message !");
         System.out.println("3rd message");
-        System.out.println("4rd message");
-        System.out.println("5rd message");
+       
 
 }

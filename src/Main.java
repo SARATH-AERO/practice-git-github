74dc2cf11a57b5a2284e180f042b0a8bfa1f2512
Sarath Sekar <sarathaero1@gmail.com>
2023-03-21 13:52:29 +0530

    nw changes added

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("2nd message !");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-    }
+        System.out.println("3rd message");
+        System.out.println("4rd message");
+        System.out.println("5rd message");
+
 }
